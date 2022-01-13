56b8e7369068a40002ec44c0c7172c40cb401222
master861 <master.key@gmx.de>
2022-01-13 17:27:11 +0100

    Add files via upload

--- src/main/java/com/bghddevelopment/LuckPemsGUI/tracks/EditTrack.java
+++ src/main/java/com/bghddevelopment/LuckPemsGUI/tracks/EditTrack.java
@@ -1,16 +1,9 @@
-/*
- * Copyright (c) BGHDDevelopment.
- * Please refer to the plugin page or GitHub page for our open-source license.
- * If you have any questions please email ceo@bghddevelopment or reach us on Discord
- */
-
-package com.bghddevelopment.LuckPemsGUI.tracks;
-
+package com.master86.Luckpermsgui.tracks;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.bghddevelopment.LuckPemsGUI.LuckPermsGUI;
+import com.master86.Luckpermsgui.Luckpermsgui;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.track.Track;
@@ -24,177 +17,177 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import com.bghddevelopment.LuckPemsGUI.util.Tools;
+import com.master86.Luckpermsgui.util.Tools;
 
 public class EditTrack implements Listener {
 
-	public static Map<Player, Track> addgroup = new HashMap<>();
-	public static Map<Player, Track> insertgroup = new HashMap<>();
-	public static Map<Player, Track> rename = new HashMap<>();
-	public static Map<Player, Track> clone = new HashMap<>();
-	static LuckPerms l = LuckPermsProvider.get();
-	
-	@EventHandler
-	public void onGroupAdd(AsyncPlayerChatEvent e) {
-		if (!addgroup.containsKey(e.getPlayer())) return;
-		String message = e.getMessage();
-		Track g = addgroup.get(e.getPlayer());
-		
-		Tools.sendCommand(e.getPlayer(), "lp track "+g.getName()+" append "+message);
-		addgroup.remove(e.getPlayer());
-		Bukkit.getScheduler().scheduleSyncDelayedTask(LuckPermsGUI.getInstance(), () -> {
-			open(e.getPlayer(), g);
-		}, 5);
-		e.setCancelled(true);
-	}
-	
-	@EventHandler
-	public void onGroupInsert(AsyncPlayerChatEvent e) {
-		if (!insertgroup.containsKey(e.getPlayer())) return;
-		String message = e.getMessage();
-		Track g = insertgroup.get(e.getPlayer());
-		
-		Tools.sendCommand(e.getPlayer(), "lp track "+g.getName()+" insert "+message);
-		insertgroup.remove(e.getPlayer());
-		Bukkit.getScheduler().scheduleSyncDelayedTask(LuckPermsGUI.getInstance(), () -> {
-			open(e.getPlayer(), g);
-		}, 5);
-		e.setCancelled(true);
-	}
-	
-	@EventHandler
-	public void onRename(AsyncPlayerChatEvent e) {
-		if (!rename.containsKey(e.getPlayer())) return;
-		String message = e.getMessage();
-		Track g = rename.get(e.getPlayer());
-		
-		Tools.sendCommand(e.getPlayer(), "lp track "+g.getName()+" rename "+message);
-		rename.remove(e.getPlayer());
-		Bukkit.getScheduler().scheduleSyncDelayedTask(LuckPermsGUI.getInstance(), () -> {
-			open(e.getPlayer(), g);
-		}, 5);
-		e.setCancelled(true);
-	}
-	
-	@EventHandler
-	public void onClone(AsyncPlayerChatEvent e) {
-		if (!clone.containsKey(e.getPlayer())) return;
-		String message = e.getMessage();
-		Track g = clone.get(e.getPlayer());
-		
-		Tools.sendCommand(e.getPlayer(), "lp track "+g.getName()+" clone "+message);
-		clone.remove(e.getPlayer());
-		Bukkit.getScheduler().scheduleSyncDelayedTask(LuckPermsGUI.getInstance(), () -> {
-			open(e.getPlayer(), g);
-		}, 5);
-		e.setCancelled(true);
-	}
+    public static Map<Player, Track> addgroup = new HashMap<>();
+    public static Map<Player, Track> insertgroup = new HashMap<>();
+    public static Map<Player, Track> rename = new HashMap<>();
+    public static Map<Player, Track> clone = new HashMap<>();
+    static LuckPerms l = LuckPermsProvider.get();
 
-	public static void open(Player p, Track group) {
-		Inventory myInventory = Bukkit.createInventory(null, 54, ChatColor.AQUA+"LuckPerms track");
-		Tools.onAsync(() -> {
-			
-			ItemStack info = Tools.button(Material.ARMOR_STAND,
-					"&6Info",
-					Arrays.asList(
-							"&cName: &e"+group.getName(),
-							"&cAll groups: &e"+group.getGroups().size()),
-					1);
-			myInventory.setItem(4, info);
-			// ----------------------- INFO ------------------------------
-	
-			// ----------------------- PERMISSION ------------------------------
-			ItemStack addnewgroup = Tools.button(Material.TORCH, "&6Add group", Arrays.asList("&eClick here to add a group"), 1);
-			myInventory.setItem(0, addnewgroup);
-			
-			ItemStack insertgroup = Tools.button(Material.ARROW, "&6Insert group", Arrays.asList("&eClick to insert a group"), 1);
-			myInventory.setItem(1, insertgroup);
-			
-			ItemStack clear = Tools.button(Material.REDSTONE_BLOCK, "&cClear", Arrays.asList("&eRemove all groups"), 1);
-			myInventory.setItem(2, clear);
+    @EventHandler
+    public void onGroupAdd(AsyncPlayerChatEvent e) {
+        if (!addgroup.containsKey(e.getPlayer())) return;
+        String message = e.getMessage();
+        Track g = addgroup.get(e.getPlayer());
 
-			ItemStack delete = Tools.button(Material.REDSTONE, "&cDelete", Arrays.asList("&eClick to delete the track"), 1);
-			myInventory.setItem(5, delete);
-			
-			ItemStack rename = Tools.button(Material.NAME_TAG, "&6Rename", Arrays.asList("&eClick to rename the track"), 1);
-			myInventory.setItem(6, rename);
+        Tools.sendCommand(e.getPlayer(), "lp track "+g.getName()+" append "+message);
+        addgroup.remove(e.getPlayer());
+        Bukkit.getScheduler().scheduleSyncDelayedTask(Luckpermsgui.plugin, () -> {
+            open(e.getPlayer(), g);
+        }, 5);
+        e.setCancelled(true);
+    }
 
-			ItemStack clone = Tools.button(Material.PAPER, "&6Clone", Arrays.asList("&eClick to clone the track"), 1);
-			myInventory.setItem(7, clone);
-			// ----------------------- PERMISSION ------------------------------
-	
-			ItemStack back = Tools.button(Material.BARRIER, "&6Back", Arrays.asList(""), 1);
-			myInventory.setItem(8, back);
-			
-			
-			int sk = 9;
-			for (String g : group.getGroups()) {
-				ItemStack item = Tools.button(Material.TNT, "&6"+g, Arrays.asList("&cClick to remove"), 1);
-				myInventory.setItem(sk, item);
-				sk++;
-			}
-		});
-		p.openInventory(myInventory);
-	}
-	
-	@EventHandler
-	public void onInventoryClickEvent(InventoryClickEvent e) {
-		Player p = (Player) e.getWhoClicked();
+    @EventHandler
+    public void onGroupInsert(AsyncPlayerChatEvent e) {
+        if (!insertgroup.containsKey(e.getPlayer())) return;
+        String message = e.getMessage();
+        Track g = insertgroup.get(e.getPlayer());
 
-		Inventory inv = e.getClickedInventory();
-		ItemStack item = e.getCurrentItem();
-		if (inv != null && item != null)
-			if (e.getView().getTitle().equals(ChatColor.AQUA+"LuckPerms track")) {
-				e.setCancelled(true);
-				if (item.hasItemMeta())
-					if (item.getItemMeta().hasDisplayName()) {
-						
-						String group = ChatColor.stripColor(inv.getItem(4).getItemMeta().getLore().get(0).split(" ")[1]);
-						Track g = l.getTrackManager().getTrack(group);
-						
-						String name = ChatColor.stripColor(item.getItemMeta().getDisplayName());
-						if (name.equals("Back")) {
-							TracksGUI.open(p);
-						} else if (name.equals("Add group")) {
-							Tools.sendMessage(p, "&eWrite in chat:");
-							Tools.sendMessage(p, "&8<&7Group&8>");
-							Tools.sendMessage(p, "&aGroup - The group you want to add");
-							addgroup.put(p, g);
-							p.closeInventory();
-						} else if (name.equals("Insert group")) {
-							Tools.sendMessage(p, "&eWrite in chat:");
-							Tools.sendMessage(p, "&8<&7Group&8> &8<&7Position&8>");
-							Tools.sendMessage(p, "&aGroup - The group you want to add");
-							Tools.sendMessage(p, "&aPosition - The position to insert the group at");
-							insertgroup.put(p, g);
-							p.closeInventory();
-						} else if (name.equals("Clear")) {
-							g.clearGroups();
-							open(p, g);
-						} else if (name.equals("Delete")) {
-							Tools.sendCommand(p, "lp deletetrack "+g.getName());
-							Bukkit.getScheduler().runTaskLater(LuckPermsGUI.getInstance(), () -> {
-								TracksGUI.open(p);
-							}, 5);
-						} else if (name.equals("Rename")) {
-							Tools.sendMessage(p, "&eWrite in chat:");
-							Tools.sendMessage(p, "&8<&7Name&8>");
-							Tools.sendMessage(p, "&aName - The new name of the track");
-							rename.put(p, g);
-							p.closeInventory();
-						} else if (name.equals("Clone")) {
-							Tools.sendMessage(p, "&eWrite in chat:");
-							Tools.sendMessage(p, "&8<&7Name&8>");
-							Tools.sendMessage(p, "&aName - The name of the new track");
-							clone.put(p, g);
-							p.closeInventory();
-						} else if (item.getType().equals(Material.TNT)) {
-							Tools.sendCommand(p, "lp track "+g.getName()+" remove "+ChatColor.stripColor(item.getItemMeta().getDisplayName()));
-							Bukkit.getScheduler().runTaskLater(LuckPermsGUI.getInstance(), () -> {
-								open(p, g);
-							}, 5);
-						}
-					}
-			}
-	}
+        Tools.sendCommand(e.getPlayer(), "lp track "+g.getName()+" insert "+message);
+        insertgroup.remove(e.getPlayer());
+        Bukkit.getScheduler().scheduleSyncDelayedTask(Luckpermsgui.plugin, () -> {
+            open(e.getPlayer(), g);
+        }, 5);
+        e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onRename(AsyncPlayerChatEvent e) {
+        if (!rename.containsKey(e.getPlayer())) return;
+        String message = e.getMessage();
+        Track g = rename.get(e.getPlayer());
+
+        Tools.sendCommand(e.getPlayer(), "lp track "+g.getName()+" rename "+message);
+        rename.remove(e.getPlayer());
+        Bukkit.getScheduler().scheduleSyncDelayedTask(Luckpermsgui.plugin, () -> {
+            open(e.getPlayer(), g);
+        }, 5);
+        e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onClone(AsyncPlayerChatEvent e) {
+        if (!clone.containsKey(e.getPlayer())) return;
+        String message = e.getMessage();
+        Track g = clone.get(e.getPlayer());
+
+        Tools.sendCommand(e.getPlayer(), "lp track "+g.getName()+" clone "+message);
+        clone.remove(e.getPlayer());
+        Bukkit.getScheduler().scheduleSyncDelayedTask(Luckpermsgui.plugin, () -> {
+            open(e.getPlayer(), g);
+        }, 5);
+        e.setCancelled(true);
+    }
+
+    public static void open(Player p, Track group) {
+        Inventory myInventory = Bukkit.createInventory(null, 54, ChatColor.AQUA+"LuckPerms track");
+        Tools.onAsync(() -> {
+
+            ItemStack info = Tools.button(Material.ARMOR_STAND,
+                    "&6Info",
+                    Arrays.asList(
+                            "&cName: &e"+group.getName(),
+                            "&cAll groups: &e"+group.getGroups().size()),
+                    1);
+            myInventory.setItem(4, info);
+            // ----------------------- INFO ------------------------------
+
+            // ----------------------- PERMISSION ------------------------------
+            ItemStack addnewgroup = Tools.button(Material.TORCH, "&6Add group", Arrays.asList("&eClick here to add a group"), 1);
+            myInventory.setItem(0, addnewgroup);
+
+            ItemStack insertgroup = Tools.button(Material.ARROW, "&6Insert group", Arrays.asList("&eClick to insert a group"), 1);
+            myInventory.setItem(1, insertgroup);
+
+            ItemStack clear = Tools.button(Material.REDSTONE_BLOCK, "&cClear", Arrays.asList("&eRemove all groups"), 1);
+            myInventory.setItem(2, clear);
+
+            ItemStack delete = Tools.button(Material.REDSTONE, "&cDelete", Arrays.asList("&eClick to delete the track"), 1);
+            myInventory.setItem(5, delete);
+
+            ItemStack rename = Tools.button(Material.NAME_TAG, "&6Rename", Arrays.asList("&eClick to rename the track"), 1);
+            myInventory.setItem(6, rename);
+
+            ItemStack clone = Tools.button(Material.PAPER, "&6Clone", Arrays.asList("&eClick to clone the track"), 1);
+            myInventory.setItem(7, clone);
+            // ----------------------- PERMISSION ------------------------------
+
+            ItemStack back = Tools.button(Material.BARRIER, "&6Back", Arrays.asList(""), 1);
+            myInventory.setItem(8, back);
+
+
+            int sk = 9;
+            for (String g : group.getGroups()) {
+                ItemStack item = Tools.button(Material.TNT, "&6"+g, Arrays.asList("&cClick to remove"), 1);
+                myInventory.setItem(sk, item);
+                sk++;
+            }
+        });
+        p.openInventory(myInventory);
+    }
+
+    @EventHandler
+    public void onInventoryClickEvent(InventoryClickEvent e) {
+        Player p = (Player) e.getWhoClicked();
+
+        Inventory inv = e.getClickedInventory();
+        ItemStack item = e.getCurrentItem();
+        if (inv != null && item != null)
+            if (e.getView().getTitle().equals(ChatColor.AQUA+"LuckPerms track")) {
+                e.setCancelled(true);
+                if (item.hasItemMeta())
+                    if (item.getItemMeta().hasDisplayName()) {
+
+                        String group = ChatColor.stripColor(inv.getItem(4).getItemMeta().getLore().get(0).split(" ")[1]);
+                        Track g = l.getTrackManager().getTrack(group);
+
+                        String name = ChatColor.stripColor(item.getItemMeta().getDisplayName());
+                        if (name.equals("Back")) {
+                            TracksGUI.open(p);
+                        } else if (name.equals("Add group")) {
+                            Tools.sendMessage(p, "&eWrite in chat:");
+                            Tools.sendMessage(p, "&8<&7Group&8>");
+                            Tools.sendMessage(p, "&aGroup - The group you want to add");
+                            addgroup.put(p, g);
+                            p.closeInventory();
+                        } else if (name.equals("Insert group")) {
+                            Tools.sendMessage(p, "&eWrite in chat:");
+                            Tools.sendMessage(p, "&8<&7Group&8> &8<&7Position&8>");
+                            Tools.sendMessage(p, "&aGroup - The group you want to add");
+                            Tools.sendMessage(p, "&aPosition - The position to insert the group at");
+                            insertgroup.put(p, g);
+                            p.closeInventory();
+                        } else if (name.equals("Clear")) {
+                            g.clearGroups();
+                            open(p, g);
+                        } else if (name.equals("Delete")) {
+                            Tools.sendCommand(p, "lp deletetrack "+g.getName());
+                            Bukkit.getScheduler().runTaskLater(Luckpermsgui.plugin, () -> {
+                                TracksGUI.open(p);
+                            }, 5);
+                        } else if (name.equals("Rename")) {
+                            Tools.sendMessage(p, "&eWrite in chat:");
+                            Tools.sendMessage(p, "&8<&7Name&8>");
+                            Tools.sendMessage(p, "&aName - The new name of the track");
+                            rename.put(p, g);
+                            p.closeInventory();
+                        } else if (name.equals("Clone")) {
+                            Tools.sendMessage(p, "&eWrite in chat:");
+                            Tools.sendMessage(p, "&8<&7Name&8>");
+                            Tools.sendMessage(p, "&aName - The name of the new track");
+                            clone.put(p, g);
+                            p.closeInventory();
+                        } else if (item.getType().equals(Material.TNT)) {
+                            Tools.sendCommand(p, "lp track "+g.getName()+" remove "+ChatColor.stripColor(item.getItemMeta().getDisplayName()));
+                            Bukkit.getScheduler().runTaskLater(Luckpermsgui.plugin, () -> {
+                                open(p, g);
+                            }, 5);
+                        }
+                    }
+            }
+    }
 }
--- src/main/java/com/bghddevelopment/LuckPemsGUI/tracks/TracksGUI.java
+++ src/main/java/com/bghddevelopment/LuckPemsGUI/tracks/TracksGUI.java
@@ -1,17 +1,11 @@
-/*
- * Copyright (c) BGHDDevelopment.
- * Please refer to the plugin page or GitHub page for our open-source license.
- * If you have any questions please email ceo@bghddevelopment or reach us on Discord
- */
-
-package com.bghddevelopment.LuckPemsGUI.tracks;
-
+package com.master86.Luckpermsgui.tracks;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import com.bghddevelopment.LuckPemsGUI.LuckPermsGUI;
-import com.bghddevelopment.LuckPemsGUI.util.OpenGUI;
+
+import com.master86.Luckpermsgui.util.OpenGUI;
+import com.master86.Luckpermsgui.Luckpermsgui;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.track.Track;
@@ -25,71 +19,71 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import com.bghddevelopment.LuckPemsGUI.util.Tools;
+import com.master86.Luckpermsgui.util.Tools;
 
 public class TracksGUI implements Listener {
 
-	List<Player> newGroup = new ArrayList<>();
-	
-	@EventHandler
-	public void onGroupAdd(AsyncPlayerChatEvent e) {
-		if (!newGroup.contains(e.getPlayer())) return;
-		String message = e.getMessage();
-		
-		Tools.sendCommand(e.getPlayer(), "lp createtrack "+message);
-		newGroup.remove(e.getPlayer());
-		Bukkit.getScheduler().scheduleSyncDelayedTask(LuckPermsGUI.getInstance(), () -> {
-			open(e.getPlayer());
-		}, 5);
-		e.setCancelled(true);
-	}
+    List<Player> newGroup = new ArrayList<>();
 
-	static LuckPerms l = LuckPermsProvider.get();
+    @EventHandler
+    public void onGroupAdd(AsyncPlayerChatEvent e) {
+        if (!newGroup.contains(e.getPlayer())) return;
+        String message = e.getMessage();
 
-	public static void open(Player p) {
-		Inventory myInventory = Bukkit.createInventory(null, 54, ChatColor.AQUA+"LuckPerms tracks");
-		Tools.onAsync(() -> {
-		
-		int sk = 9;
-		for (Track group : l.getTrackManager().getLoadedTracks()) {
-			String name = group.getName();
-			ItemStack item = Tools.button(Material.TNT, "&6"+name, Arrays.asList("&ePress to edit this track"), 1);
-			myInventory.setItem(sk, item);
-			sk++;
-		}
+        Tools.sendCommand(e.getPlayer(), "lp createtrack "+message);
+        newGroup.remove(e.getPlayer());
+        Bukkit.getScheduler().scheduleSyncDelayedTask(Luckpermsgui.plugin, () -> {
+            open(e.getPlayer());
+        }, 5);
+        e.setCancelled(true);
+    }
 
-		ItemStack back = Tools.button(Material.BARRIER, "&6Back", Arrays.asList(""), 1);
-		myInventory.setItem(8, back);
+    static LuckPerms l = LuckPermsProvider.get();
 
-		ItemStack newGroup = Tools.button(Material.PAPER, "&6New track", Arrays.asList("&eCreate new track"), 1);
-		myInventory.setItem(0, newGroup);
-		
-		});
-		p.openInventory(myInventory);
-	}
-	
-	@EventHandler
-	public void onInventoryClickEvent(InventoryClickEvent e) {
-		Player p = (Player) e.getWhoClicked();
+    public static void open(Player p) {
+        Inventory myInventory = Bukkit.createInventory(null, 54, ChatColor.AQUA+"LuckPerms tracks");
+        Tools.onAsync(() -> {
 
-		Inventory inv = e.getClickedInventory();
-		ItemStack item = e.getCurrentItem();
-		if (inv != null && item != null)
-			if (e.getView().getTitle().equals(ChatColor.AQUA+"LuckPerms tracks")) {
-				e.setCancelled(true);
-				if (item.hasItemMeta())
-					if (item.getItemMeta().hasDisplayName()) {
-						String group = ChatColor.stripColor(item.getItemMeta().getDisplayName());
-						if (group.equals("Back")) {
-							OpenGUI.openGUI(p);
-						} else if (group.equals("New track")) {
-							Tools.sendMessage(p, "&eWrite in chat:");
-							Tools.sendMessage(p, "&8<&7Name&8>");
-							Tools.sendMessage(p, "&aName - Track name");
-							newGroup.add(p);
-							p.closeInventory();
-						} else EditTrack.open(p, l.getTrackManager().getTrack(group));
-					}
-			}
-	}
+            int sk = 9;
+            for (Track group : l.getTrackManager().getLoadedTracks()) {
+                String name = group.getName();
+                ItemStack item = Tools.button(Material.TNT, "&6"+name, Arrays.asList("&ePress to edit this track"), 1);
+                myInventory.setItem(sk, item);
+                sk++;
+            }
+
+            ItemStack back = Tools.button(Material.BARRIER, "&6Back", Arrays.asList(""), 1);
+            myInventory.setItem(8, back);
+
+            ItemStack newGroup = Tools.button(Material.PAPER, "&6New track", Arrays.asList("&eCreate new track"), 1);
+            myInventory.setItem(0, newGroup);
+
+        });
+        p.openInventory(myInventory);
+    }
+
+    @EventHandler
+    public void onInventoryClickEvent(InventoryClickEvent e) {
+        Player p = (Player) e.getWhoClicked();
+
+        Inventory inv = e.getClickedInventory();
+        ItemStack item = e.getCurrentItem();
+        if (inv != null && item != null)
+            if (e.getView().getTitle().equals(ChatColor.AQUA+"LuckPerms tracks")) {
+                e.setCancelled(true);
+                if (item.hasItemMeta())
+                    if (item.getItemMeta().hasDisplayName()) {
+                        String group = ChatColor.stripColor(item.getItemMeta().getDisplayName());
+                        if (group.equals("Back")) {
+                            OpenGUI.openGUI(p);
+                        } else if (group.equals("New track")) {
+                            Tools.sendMessage(p, "&eWrite in chat:");
+                            Tools.sendMessage(p, "&8<&7Name&8>");
+                            Tools.sendMessage(p, "&aName - Track name");
+                            newGroup.add(p);
+                            p.closeInventory();
+                        } else EditTrack.open(p, l.getTrackManager().getTrack(group));
+                    }
+            }
+    }
 }
